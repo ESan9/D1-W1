503b540bc0823b1344d08f4649ca479727bed58a
EmanSan9 <sanna950@gmail.com>
2025-09-08 17:15:13 +0200

    V2

--- src/Es3.java
+++ src/Es3.java
@@ -3,11 +3,21 @@ public class Es3 {
         return (l1 + l2) * 2;
     }
 
+    public static int pari(int int1) {
+        if (int1 % 2 == 0) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
     public static void main(String[] args) {
         double l1 = 1;
         double l2 = 2;
         double result = perimeter(l1, l2);
         System.out.println(result);
+        int num = 4;
+        System.out.println(pari(num));
 
     }
 }
