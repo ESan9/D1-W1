7355e91f99aae2febf5e7bfef0d4eb3e54612b56
EmanSan9 <sanna950@gmail.com>
2025-09-08 16:49:33 +0200

    V2

--- src/Es2.java
+++ src/Es2.java
@@ -1,2 +1,16 @@
+import java.util.Scanner;
+
 public class Es2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Inserisci la prima stringa");
+        String name1 = scanner.nextLine();
+        System.out.println("Inserisci la seconda stringa");
+        String name2 = scanner.nextLine();
+        System.out.println("Inserisci la terza stringa");
+        String name3 = scanner.nextLine();
+        System.out.println(name1 + name2 + name3);
+        System.out.println(name3 + name2 + name1);
+
+    }
 }
