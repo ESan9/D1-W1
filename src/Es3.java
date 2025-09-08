14ae13d1c373de4517d3d14744b4c67396132dfc
EmanSan9 <sanna950@gmail.com>
2025-09-08 16:56:54 +0200

    V3

--- src/Es3.java
+++ src/Es3.java
@@ -1,2 +1,13 @@
 public class Es3 {
+    public static double perimeter(double l1, double l2) {
+        return (l1 + l2) * 2;
+    }
+
+    public static void main(String[] args) {
+        double l1 = 1;
+        double l2 = 2;
+        double result = perimeter(l1, l2);
+        System.out.println(result);
+
+    }
 }
